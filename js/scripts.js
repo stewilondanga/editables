2336e838fdadb7737e9d551871617904f9064ee4
Stewart ilondanga <stilondanga@gmail.com>
2018-01-03 15:04:29 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@ var $TABLE = $('#table');
 var $BTN = $('#export-btn');
 var $EXPORT = $('#export');
 
+$('.table-add').click(function () {
+  var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
+  $TABLE.find('table').append($clone);
+});
+
 
 
 var navigate = (function() {
